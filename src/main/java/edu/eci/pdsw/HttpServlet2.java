7a488c0bfd72a68fa3ae62c26a65b1b0dbbeef64
Alejandro Anzola <anzola95avila@gmail.com>
2017-02-14 02:58:05 -0500

    implementacion del metodo doPost igual al metodo doGet

--- src/main/java/edu/eci/pdsw/HttpServlet2.java
+++ src/main/java/edu/eci/pdsw/HttpServlet2.java
@@ -19,7 +19,16 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author fabian
+ * Ingrese los datos y verifique los resultados. Cambie el formulario para que 
+ * ahora en lugar de POST, use el método GET. Qué diferencia observa?
+ * 
+ * La diferencia que se observa es que cuando la consulta se realiza con el 
+ * metodo GET, la consulta se puede ver en la barra del navegador, mientras que
+ * en la otra permanece oculto, y solo es conocida por el navegador a la hora
+ * de enviar la informacion, despues el unico que conoce la informacion es el
+ * servidor.
+ * 
+ * @author fabian y skinman95
  */
 
 @WebServlet(
@@ -32,7 +41,7 @@ public class HttpServlet2 extends HttpServlet{
         Writer responseWriter = resp.getWriter();
         try {
             DataSourceStub data = DataSourceStub.getInstance();
-            Client client = data.getClientById(  Integer.parseInt(req.getParameter("id")) );
+            Client client = data.getClientById(  Integer.parseInt(req.getParameter("fid")) );
             responseWriter.write("<html>\n" +
 "  <head>\n" +
 "  </head>\n" +
@@ -71,6 +80,44 @@ public class HttpServlet2 extends HttpServlet{
     }
     
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+        Writer responseWriter = resp.getWriter();
+        try {
+            DataSourceStub data = DataSourceStub.getInstance();
+            Client client = data.getClientById(  Integer.parseInt(req.getParameter("fid")) );
+            responseWriter.write("<html>\n" +
+"  <head>\n" +
+"  </head>\n" +
+"\n" +
+"  <style>\n" +
+"    table, th, td {\n" +
+"    border: 1px solid black;\n" +
+"    }\n" +
+"  </style>\n" +
+"  \n" +
+"  <body>\n" +
+"    <table style=\"width:100%\">\n" +
+"      <tr>\n" +
+"	<th> Nombre </th>\n" +
+"	<th> Salario </th>\n" +
+"	<th> Direccion </th>\n" +
+"	<th> Email </th>\n" +
+"      </tr>\n" +
+"      <tr>\n" +
+"	<th> "+client.getName()+" </th>\n" +
+"	<th> "+client.getSallary()+" </th>\n" +
+"	<th> "+client.getAddress()+"</th>\n" +
+"	<th> "+client.getEmail()+" </th>\n" +
+"      </tr>\n" +
+"    </table>\n" +
+"\n" +
+"  </body>\n" +
+"</html>");
+            resp.setStatus(200);
+            
+        } catch (ClientNotFoundException e){
+            Logger.logMsg(Logger.ERROR, e.getMessage());
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "No existe un cliente con el identificador dado.");
+            resp.flushBuffer();
+        }
     }
 }
